11f5d2ac524458dfec9762d98475583cb57403ab
Alperen Elhan <alperen@elhan.io>
2020-08-21 04:30:53 +0300

    chore: remove keybinder dep

--- .ts-for-girrc.js
+++ .ts-for-girrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   environments: ['gjs'],
-  modules: ['Gtk-3.0', 'St-1.0', 'Shell-0.1', 'Wnck-3.0', 'Keybinder-3.0'],
+  modules: ['Gtk-3.0', 'St-1.0', 'Shell-0.1', 'Wnck-3.0'],
   prettify: true,
   girDirectories: [
     '/usr/share/gir-1.0',
